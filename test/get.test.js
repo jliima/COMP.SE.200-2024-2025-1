edef5cb5e8bfde09398d68637664636b3d698e67
Jere Liimatainen <jere.liimatainen@tuni.fi>
2024-12-11 17:04:33 +0200

    Enhanced get.test.js

--- test/get.test.js
+++ test/get.test.js
@@ -32,11 +32,18 @@ describe('get', () => {
       expect(get(undefined, 'a[0].b.c',)).to.be.undefined
     })
 
-    it('should return undefined if path is something else than Array|string', () => {
-      expect(get(object, 2)).to.be.undefined
-      expect(get(object, undefined)).to.be.undefined
-      expect(get(object, { a: 2})).to.be.undefined
+    it('should return undefined if path is null or undefined', () => {
       expect(get(object, null)).to.be.undefined
+      expect(get(object, undefined)).to.be.undefined
+    })
+
+    it('should return undefined if path is a Number', () => {
+      expect(get(object, 2)).to.be.undefined
+      expect(get(object, 5.1)).to.be.undefined
+    })
+
+    it('should return undefined if path is an Array-like object', () => {
+      expect(get(object, { length: 4, 0: 'a', 1: '0', 2: 'b', 3: 'c'})).to.be.undefined
     })
   })
 
